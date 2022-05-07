25c12fc2e7acaa60032cb1f170ca32907eb5dd00
Yatharth Jain <yatharthjain41@gmail.com>
2022-05-07 21:42:51 +0530

    Day 3 Commits

--- website/auth.py
+++ website/auth.py
@@ -5,8 +5,16 @@ auth=Blueprint('auth',__name__)
 @auth.route('/login/<user>',methods=['GET','POST'])
 def login(user):
     print("\n\n\n\n\n",user,"\n\n\n\n\n")
-    return render_template('popup.html')
+    return render_template('login.html')
 
 @auth.route('/student-register',methods=['GET','POST'])
 def studentreg():
+    if request.method=='POST':
+        sfname=request.form['sfname']
+        slname=request.form['slname']
+        sbranch=request.form['sbranch']
+        syear=request.form['syear'] 
+        sdob=request.form['sdob'].split('-')                               # YYYY-MM-DD
+
+        print(f'{sfname}<-->{slname}<-->{sbranch}<-->{syear}<-->{sdob}')
     return render_template('student-register.html')

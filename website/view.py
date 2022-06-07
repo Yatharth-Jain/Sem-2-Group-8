bc2ae1fe41ae666511c997ed0424decbf1b5fc6c
Yatharth Jain <yatharthjain41@gmail.com>
2022-06-07 12:02:58 +0530

    Minor Changes

--- website/view.py
+++ website/view.py
@@ -25,7 +25,7 @@ def admhome():
 @view.route('/teacher-home')
 @loginchecker(role='teacher')
 def tchhome():
-    return "<h1 style='font-size:100px;'>Teacher Home</h1>"
+    return redirect(url_for("view.classselect"))
 
 
 @view.route('/testprofilesadd')
@@ -47,50 +47,9 @@ def addtest():
     return redirect('/')
 
 
-# @view.route('/sheet', methods=['GET', 'POST'])
-# def sheet():
-#     asss = ['assi1', 'assi2', 'assi3', 'assi4', 'assi5','assi6']
-#     names = ['name1', 'name2', 'name3', 'name4', 'name5']
-#     markdict = {}
-#     total = {}
-#     for name in names:
-#         t = 0
-#         for ass in asss:
-#             mid = f'{ass}-{name}'
-#             old_mark = Marks.query.filter_by(mid=mid).first()
-#             if old_mark:
-#                 markdict[mid] = int(old_mark.mark)
-#                 t = t+markdict[mid]
-#             else:
-#                 markdict[mid] = 0
-#                 new_marks = Marks(mid=mid, mark=0)
-#                 db.session.add(new_marks)
-#                 db.session.commit()
-#         total[name] = t
-#     print(markdict)
-#     if request.method == 'POST':
-#         for name in names:
-#             for ass in asss:
-#                 mid = f'{ass}-{name}'
-#                 marks = request.form[mid]
-#                 if not marks:
-#                     marks = 0
-#                 old_mark = Marks.query.filter_by(mid=mid).first()
-
-#                 total[name] = total[name]-int(old_mark.mark)
-
-#                 old_mark.mark = marks
-
-#                 total[name] = total[name]+int(marks)
-#                 markdict[mid] = marks
-#                 db.session.commit()
-#         redirect('/sheet')
-
-#     return render_template('sheet.html', asss=asss, names=names, markdict=markdict, total=total)
-
 
 @view.route("/sheet-create",methods=['GET','POST'])
-def testform():
+def classselect():
     form1=ClassForm()
     form1.year.choices=[(yr.year,yr.year) for yr in Years.query.all()]
     if request.method=='POST':
